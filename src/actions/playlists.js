4efc1614ccb17cdb0ca291ce1ab2cfa7f2f43498
DavidB59 <david.behal@net-c.com>
2018-11-18 11:52:46 +0100

    restored url

--- src/actions/playlists.js
+++ src/actions/playlists.js
@@ -4,7 +4,7 @@ export const EVENTS_FETCHED = 'EVENTS_FETCHED'
 export const EVENT_FETCHED = 'EVENT_FETCHED'
 export const EVENT_DELETE_SUCCESS= 'EVENT_DELETE_SUCCESS'
 
-const baseUrl = 'postgres://qzieokwmdtvowh:188f62abfbf6a7451b3ddc03c3386b09865b69edb4de8afc8f2204ee2681cab8@ec2-54-75-231-3.eu-west-1.compute.amazonaws.com:5432/ddbn56g37im49d' || 'http://localhost:4000'
+const baseUrl = 'http://localhost:4000'
 const eventsFetched = playlists => ({
   type: EVENTS_FETCHED,
   playlists
@@ -13,6 +13,7 @@ const eventsFetched = playlists => ({
 export const loadPlaylists = () => (dispatch, getState) => {
   if (getState().playlists) return 
   console.log('loading playlistS')
+  console.log(process.env.DATABASE_URL)
   console.log(baseUrl)
   request(`${baseUrl}/playlists`)
     .then(response => {
