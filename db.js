562d45b9aaef990bae0a6b3c031ee1edd1ae8f6b
DavidB59 <david.behal@net-c.com>
2018-11-18 10:56:13 +0100

    r

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 const Sequelize = require('sequelize')
 
-const connectionString = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
+const connectionString = 'postgres://qzieokwmdtvowh:188f62abfbf6a7451b3ddc03c3386b09865b69edb4de8afc8f2204ee2681cab8@ec2-54-75-231-3.eu-west-1.compute.amazonaws.com:5432/ddbn56g37im49d' || 'http://localhost:4000'
+
+//
+//const connectionString = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
 const sequelize = new Sequelize(connectionString, {define: { timestamps: false }})
 
 sequelize.sync({/*force:true*/} )
