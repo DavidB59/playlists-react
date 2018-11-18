09d86076bb3911be4b234991eda0fafc17a99c79
DavidB59 <david.behal@net-c.com>
2018-11-18 10:42:33 +0100

    url test

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -3,7 +3,7 @@
 
  export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
  
- const baseUrl = 'http://localhost:4000'
+ const baseUrl = 'postgres://qzieokwmdtvowh:188f62abfbf6a7451b3ddc03c3386b09865b69edb4de8afc8f2204ee2681cab8@ec2-54-75-231-3.eu-west-1.compute.amazonaws.com:5432/ddbn56g37im49d' || 'http://localhost:4000'
  
  const loginSuccess = (jwt) => ({
    type: LOGIN_SUCCESS,
