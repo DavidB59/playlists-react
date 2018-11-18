3af85c94f2d77a08d2701b0b54bf58bd4abd699c
DavidB59 <david.behal@net-c.com>
2018-11-18 10:47:13 +0100

    r

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,6 +1,6 @@
 import request from 'superagent'
 
-const baseUrl = process.env.DATABASE_URL || 'http://localhost:4000'
+const baseUrl = 'postgres://qzieokwmdtvowh:188f62abfbf6a7451b3ddc03c3386b09865b69edb4de8afc8f2204ee2681cab8@ec2-54-75-231-3.eu-west-1.compute.amazonaws.com:5432/ddbn56g37im49d' || 'http://localhost:4000'
 
 
 export const createUser = (data) => dispatch => {
